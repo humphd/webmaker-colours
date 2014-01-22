efe1aab5864a49f8d4b83e16c9aa4388a227f676
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2014-01-22 16:44:06 -0500

    Fix handling of meta element properties

--- data/webmaker-colours.js
+++ data/webmaker-colours.js
@@ -33,6 +33,99 @@
     return srcs;
   }
 
+  var twitterRegex = /^twitter\:/,
+      ogRegex = /^og\:/,
+      dcRegex = /^dc\./,
+      dctermsRegex = /^dcterms\./;
+  function parseMeta() {
+    /**
+     * Find social graph and other metadata we might care about:
+     *   <meta property="og:title" content="This is a title">
+     *   <meta name="og:url" content="http://foo.com">
+     *   <meta name="twitter:title" content="This is also a title">
+     */
+    var metas = document.querySelectorAll("meta"),
+        result = {},
+        unprocessed = [],
+        // Buckets for each namespaced type of meta data we look for
+        og = {},
+        twitter = {},
+        dc = {},
+        dcterms = {},
+        name,
+        content;
+
+    // Try grabbing social graph data first
+    for(var meta of metas) {
+      // HTML5 spec says to use `name`, OG and others say `property`.
+      name = meta.getAttribute('name') || meta.getAttribute('property');
+      if(!name) {
+        continue;
+      }
+      name = name.toLowerCase();
+
+      // In the wild you'll find name/content, property/content, name/value, ...
+      content = meta.getAttribute('content') || meta.getAttribute('value') || '';
+
+      if ( ogRegex.test( name ) ) {
+        og[ name ] = content;
+      } else if ( twitterRegex.test( name ) ) {
+        twitter[ name ] = content;
+      } else if ( dcRegex.test( name ) ) {
+        dc[ name ] = content;
+      } else if ( dctermsRegex.test( name ) ) {
+        dcterms[ name ] = content;
+      } else {
+        // Remember the ones we didn't use in case we don't get
+        // any social graph data and want to look at it later
+        unprocessed.push({
+          name: name,
+          content: content
+        });
+      }
+    }
+
+    // Attach any populated namespace buckets
+    if ( Object.keys( og ).length ) {
+      result.og = og;
+    }
+    if ( Object.keys( twitter ).length ) {
+      result.twitter = twitter;
+    }
+    if ( Object.keys( dc ).length ) {
+      result.dc = dc;
+    }
+    if ( Object.keys( dcterms ).length ) {
+      result.dcterms = dcterms;
+    }
+
+    // If we didn't find any social graph data, look for more standard metadata.
+    if ( !Object.keys( result ).length ) {
+      unprocessed.forEach( function( m ) {
+        name = m.name;
+        content = m.content;
+
+        switch( name ) {
+        case 'description':
+          result.description = content;
+          break;
+        case 'author':
+        case 'creator':
+          result.author = content;
+          break;
+        }
+      });
+    }
+
+    // Add the document title.
+    var title = document.title;
+    if ( title ) {
+      result.title = title;
+    }
+
+    return result;
+  }
+
   self.port.on("setup-ui", function(data) {
     if(hasUI) {
       self.port.emit("ui-ready");
@@ -68,6 +161,8 @@
       console.log("Interesting iframe:", srcs.iframe);
       console.log("Interesting img:", srcs.img);
       console.log("Interesting a:", srcs.a);
+
+      console.log("Interesting meta:", parseMeta());
     });
 
     self.port.on("find-elements", function() {
