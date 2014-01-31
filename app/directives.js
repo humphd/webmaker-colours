1399755d3644f0dc80fb9a69bd024dbbe61af8fd
Gvn Lazar Suntop <gavin@gsuntop.com>
2014-01-30 17:07:51 -0800

    trying to make tabswitcher into a directive

--- app/directives.js
+++ app/directives.js
@@ -12,4 +12,26 @@ angular.module('wmCollector.directives', [])
         elPreloader.attr('src', attrs.verifySrc);
       }
     };
+  })
+  .directive('tabswitcher', function () {
+    return {
+      restrict: 'AE',
+      priority: 1,
+      link: function (scope, element, attrs) {
+        var elTriggers = element[0].children;
+        var elTabbedContent = document.querySelectorAll('tabbed[group=' + attrs.control + ']')[0];
+        var elPanels = elTabbedContent.children;
+
+        angular.forEach(elPanels, function (elPanel) {
+          console.log(elPanel);
+          elPanel.style.display = 'none';
+        });
+
+        angular.forEach(elTriggers, function (elTrigger) {
+          elTrigger.addEventListener('click', function (event) {
+            console.log(event);
+          });
+        });
+      }
+    };
   });
